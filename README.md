ed979f3ed1e0fc8b43207f961dbec32e5054bb69
Guillermo Rodriguez <guillermor@mail.usf.edu>
2020-11-17 21:55:30 -0700

    Version 0.0.0.0.0.0.0.0.0.0.0.1

---
+++ README.md
@@ -0,0 +1,20 @@
+# My Personal Pomodoro app ⏰
+[Neovim is MY hero](https://img.shields.io/badge/made%20with-neovim%E2%9D%A4%EF%B8%8F-blue)
+
+<center>
+    <img src="./screenshot.png" alt="My screenshot of my project">
+</center>
+
+
+
+## Tools
+
+* Editor - [Neovim](https://neovim.io/)
+* Typography:
+    - [Comfortaa](https://fonts.google.com/specimen/Comfortaa?query=comforta)
+
+
+## TODO
+* Fix gradient on body
+* Fix settings buttons click
+* Fix button CSS on normal
--- script.js
+++ script.js
@@ -1,12 +1,52 @@
-// BUTTONS
+/* ==========================================================================
+ * timer objects is the data for the app
+ * Data flows from the DOM to change timer and back to the DOM
+======================================================================== */
+
+// DATA
+const timer = {
+  state: "pomo",
+  time: 1500,
+  pomo: "25:00",
+  short: "5:00",
+  long: "15:00",
+};
+
+// ELEMENTS
 const pomoSettings = document.getElementById("settings");
-pomoSettings.addEventListener("click", handleButtons); // Event propagation
 const pomoButtons = document.querySelectorAll(".pomodoro--button");
-
-// Main section
+const pomoString = document.getElementById("pomodoro-string");
 const mainSection = document.getElementById("main");
+const startButton = document.getElementById("start");
+const progressBar = document.getElementById('progress-bar');
+const docTitle = document.title;
+const buttonSound = new Audio('click.mp3');
 
-// Pomodoro settings buttons
+// Used to STAHP the damn timer when pressed
+var wrapper;
+
+// EVENT LISTENERS
+pomoSettings.addEventListener("click", handleButtons);
+startButton.addEventListener("click", () => {
+  buttonSound.play();
+  const { action } = startButton.dataset;
+  if (action === "start") {
+    startButton.dataset.action = "stop";
+    startButton.textContent = "stop";
+    startButton.classList.add("active-start");
+    wrapper = setInterval(() => {
+      startTimer();
+    }, 1000);
+  } else {
+    stopTimer();
+  }
+});
+
+/* ==========================================================================
+ * handleButtons(clickEvent)
+ *  updates the DOM by appending the active class to each button if clicked
+ *  passes the button state to the buttonClick function
+======================================================================== */
 function handleButtons(event) {
   // If it's the pomoButton, do the thing
   if (event.target.tagName === "BUTTON") {
@@ -26,10 +66,13 @@ function handleButtons(event) {
   }
 }
 
-// Handle button click by using the button's ID
+/* ==========================================================================
+ * buttonClick(string -> button'id')
+*   toggles pomo/short/long class on the main section to change color 
+======================================================================== */
 function buttonClick(button) {
-
-  // Remove anything first
+  // Remove any class on mainSection first
+  // Change the text then the background
   switch (button) {
     case "pomo":
       mainSection.classList.remove("short");
@@ -44,7 +87,60 @@ function buttonClick(button) {
       mainSection.classList.remove("short");
       break;
     default:
-      console.log(`Sorry, I suck...?  ${button}.`);
+      console.log(`Sorry, I suck with this...? ${button}.`);
   }
+
+  // Change state internally
+  timer["state"] = button; // pomo, short, long
+  timer["time"] = parseInt(timer[button]) * 60; // 25:00, 05:00, 15:00 -> 25, 5, 15 -> 1500, 300, 900
+  progressBar.style.width = 0; // Width of progressbar is 0
+
+  // Rerender DOM to correct time amount
+  pomoString.innerText = timer[button];
+  document.title = `${timer[button]} — ${docTitle}`;
+
+  // Add style class to main
   mainSection.classList.add(button);
+
+  stopTimer();
+}
+/* ==========================================================================
+ * startTimer()
+ *  event listener on start button that will handle the timer functionality 
+======================================================================== */
+
+function startTimer() {
+
+  // Subtract inner clock
+  timer["time"] = timer["time"] - 1;
+
+  // Manipulation to min/seconds
+  let minutes = Math.floor(timer["time"] / 60);
+  let seconds = timer["time"] % 60;
+
+  // If seconds or minutes are less than 10, redefine as double digit
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+  // RERENDER THE DOM BABY!
+  document.title = `${minutes}:${seconds} — ${docTitle}`;
+  pomoString.innerText = `${minutes}:${seconds}`;
+
+  // UPDATE THE PROGRESSBAR
+  // Fraction of time left over totaltime
+  let progress = (parseInt(timer[(timer["state"])]) * 60 - timer["time"]) / (parseInt(timer[(timer["state"])]) * 60); 
+  progress = progress * 100;
+  progressBar.style.width = `${progress}%`;
+
+  if (timer["time"] <= 0) {
+    // Redefine timer back to its original state
+    timer["time"] = parseInt(timer[(timer["state"])]) * 60;
+    return;
+  }
+}
+
+function stopTimer() {
+  clearTimeout(wrapper);
+  startButton.dataset.action = "start";
+  startButton.textContent = "start";
+  startButton.classList.remove("active-start");
 }
